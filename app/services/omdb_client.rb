bd7a4ed3fabff664aa5406295e12a132f60f117a
cryalex141 <tarasfurtak305@gmail.com>
2024-05-08 20:30:09 +0300

    Fix

--- app/services/omdb_client.rb
+++ app/services/omdb_client.rb
@@ -9,20 +9,9 @@ class OmdbClient
     begin
       response = Faraday.get(URL, {apikey: @api_key, s: string})
       parse_body(response)
-    rescue Faraday::ConnectionFailed => e
-      puts "error: #{e.message}"
-    rescue Faraday::TimeoutError => e
-      puts "error: #{e.message}"
-    rescue JSON::ParserError => e
-      puts "error: #{e.message}"
     end
   end
 
-  def search_by_title(string)
-    parse_body(
-      Faraday.get(URL, {apikey: @api_key, t: string})
-    )
-  end
 
   def find_by_id(id)
     parse_body(
