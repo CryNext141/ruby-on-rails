2e46682c6539b536ac7936ab8dc846916a7c795d
cryalex141 <tarasfurtak305@gmail.com>
2024-05-09 12:34:51 +0300

    Fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,18 @@
 class Movie < ApplicationRecord
-  GENRES = %W[action comedy drama horror romance thriller sci-fi western].freeze
+  GENRES = %w[action comedy drama horror romance thriller sci-fi western].freeze
 
   has_many :comments, as: :commentable
+  has_many :favorites, dependent: :destroy # Додано обробник залежності для автоматичного видалення пов'язаних улюблених записів
 
-  validates :title,:year_of_creation, presence: true
-  validates :title, uniqueness: {scope: %i[duration]}
+  validates :title, :year_of_creation, presence: true
+  validates :title, uniqueness: { scope: [:duration] }
   validates :description, length: { minimum: 10 }
   validate :genres_validation
 
   private
 
   def genres_validation
-    unless
-    genres.is_a?(Array) && genres.any? { |g| g.present? }
+    unless genres.is_a?(Array) && genres.any?(&:present?)
       errors.add(:genres, 'At least one must be selected')
     end
   end
